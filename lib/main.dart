d50c86682df167728c1e230f58686a447895d6f8
Code~warrior200 <61137642+code-warrior200@users.noreply.github.com>
2024-05-14 05:29:02 -0700

    git commit -m

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:my_first_app/pages/first_page.dart';
+import 'package:my_first_app/pages/home_page.dart';
+import 'package:my_first_app/pages/settings_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,6 +17,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: FirstPage(),
+      routes: {
+        '/firstpage':(context) => FirstPage(),
+        'homepage':(context) => HomePage(),
+        '/settingspage':(context) => SettingsPage(),
+      },
     );
   }
 }
--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -1,7 +1,4 @@
-// ignore_for_file: camel_case_types, unused_import
 import 'package:flutter/material.dart';
-//import 'package:my_first_app/pages/second_page.dart';
-
 class FirstPage extends StatelessWidget {
   const FirstPage({super.key});
 
@@ -13,35 +10,42 @@ class FirstPage extends StatelessWidget {
         backgroundColor: Colors.blue,
       ),
       drawer: const Drawer(
-        backgroundColor: Colors.blue,
-        child: Column(
-          children: [
-            DrawerHeader(
-              child: Icon(
-                Icons.favorite,
-                color: Colors.white,
-                size: 48,
+          backgroundColor: Colors.blue,
+          child: Column(
+            children: [
+              DrawerHeader(
+                child: Icon(
+                  Icons.favorite,
+                  color: Colors.white,
+                  size: 48,
                 ),
               ),
               //home page list title
               ListTile(
-                leading: Icon(Icons.home, color: Colors.white,),
+                leading: Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ),
                 title: Text("HOME"),
               ),
 
               ListTile(
-                leading: Icon(Icons.person, color: Colors.white,),
+                leading: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
                 title: Text("PROFILE"),
               ),
 
               ListTile(
-                leading: Icon(Icons.settings, color: Colors.white,),
+                leading: Icon(
+                  Icons.settings,
+                  color: Colors.white,
+                ),
                 title: Text("SETTINGS"),
               ),
-          ],
-        )
-      ),
-      
+            ],
+          )),
     );
   }
 }
